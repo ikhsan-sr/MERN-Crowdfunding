8c86930f067f54f9eaa3153208ded4276248cc7f
Muhammad Ikhsan <muhammad.ikhsan@baleomol.dev>
2021-12-02 12:00:06 +0700

    setup eslint airbnb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = function () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Crowdfunding MERN
-        </p>
+        <p>Crowdfunding MERN</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
